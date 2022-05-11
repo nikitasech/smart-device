68609e2b65e1b7114f855eac4bde57b157480b10
NikitaSech <NikitaSech@yandex.ru>
2022-05-12 02:39:03 +0300

    Подогнал пп блока main-footer

--- source/js/modules/accordion/init-accordions.js
+++ source/js/modules/accordion/init-accordions.js
@@ -3,9 +3,10 @@ import {Accordion} from './accordion';
 const initAccordions = () => {
   const elAccordions = document.querySelectorAll('[data-accordion]');
   const MAX_VIEWPORT_SIZE = 770;
+  const DEFAULT_OPEN_TAB = 1;
 
   for (let i = 0; i < elAccordions.length; i++) {
-    const accordion = new Accordion(elAccordions[i], MAX_VIEWPORT_SIZE);
+    const accordion = new Accordion(elAccordions[i], MAX_VIEWPORT_SIZE, DEFAULT_OPEN_TAB);
 
     accordion.init();
   }
