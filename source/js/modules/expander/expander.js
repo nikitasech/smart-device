dbec7ae5965f0cc71f708f241803e1a8aae7f6f3
NikitaSech <NikitaSech@yandex.ru>
2022-05-14 07:09:26 +0300

    Добавил смену названия кнопки при раскрытии текста в блоке about

--- source/js/modules/expander/expander.js
+++ source/js/modules/expander/expander.js
@@ -8,6 +8,11 @@ export class Expander {
   }
 
   _toggleContent() {
+    if (!this._elContent.classList.contains('is-opened')) {
+      this._elButton.textContent = 'Свернуть';
+    } else {
+      this._elButton.textContent = 'Подробнее';
+    }
     this._elContent.classList.toggle('is-opened');
   }
 
